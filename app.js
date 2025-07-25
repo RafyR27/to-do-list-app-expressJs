585ed0fcb3322ebf31a6fdabc7c1097b8455ab41
FyyyyAYEE <kucingkucingtampan@gmail.com>
2025-07-25 22:11:57 +0700

    test 6

--- app.js
+++ app.js
@@ -49,6 +49,9 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+    }),
     cookie: {
       maxAge: 1000 * 60 * 60,
     },
